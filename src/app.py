1670c4c9001cc39728f7da21e7d59e2bdce1edd1
Joe <joediaz88@hotmail.com>
2025-01-21 16:32:48 +0000

    implementados métodos de POST, DELETE, y GET

--- src/app.py
+++ src/app.py
@@ -27,16 +27,31 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def getMember(member_id):
+    member = jackson_family.get_member(member_id)
+    if member is None:
+            return jsonify({"error": "Member not found"}), 404
+    return jsonify(member), 200
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def deleteMember(member_id):
+    deleted_member = jackson_family.delete_member(member_id)
+    if deleted_member is None:
+            return jsonify({"error": "Member not found"}), 404
+    return ("ok"),200
+
+@app.route('/members', methods=['POST'])
+def add_new_member():
+    new_member = jackson_family.add_member(request.json)
+    return jsonify(new_member), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
